95f780290f30779002ed781f6d252ed520b02d69
unknown <bkabuye@BKABUYEL4.3amlabs.net>
2016-02-25 10:40:57 -0500

    Changes to signup and sign in home screen

--- ProTeam2/app/src/main/java/com/example/brian_pc/proteam/MainActivity.java
+++ ProTeam2/app/src/main/java/com/example/brian_pc/proteam/MainActivity.java
@@ -1,32 +1,42 @@
 package com.example.brian_pc.proteam;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {/*
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.homevertical);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    ImageButton signIn, signUp;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    */}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        signIn = (ImageButton) findViewById(R.id.SignIn);
+        signUp = (ImageButton) findViewById(R.id.SignUp);
+      View.OnClickListener handler = new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              if(v == signIn) {
+                  Intent startNewActivity = new Intent(MainActivity.this, SignInActivity.class);
+                   startActivity(startNewActivity);
+                    Log.i("Content", "SignIn home");
+              }
+               else{
+                  Intent startNewActivity = new Intent(MainActivity.this, SignUpActivity.class);
+                  startActivity(startNewActivity);
+                  Log.i("Content", "SignUp home");
+              }
+          }
+      };
+        signUp.setOnClickListener(handler);
+        signIn.setOnClickListener(handler);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
